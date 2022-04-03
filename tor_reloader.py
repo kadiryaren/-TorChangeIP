3267b7252b52ef33d69d39b6dbbc8830fbe3cf82
Kadir Yaren <uraniumstar20@gmail.com>
2022-04-03 17:30:46 +0300

    initialized

---
+++ tor_reloader.py
@@ -0,0 +1,61 @@
+# Tor Project Ip Changer For Web Scraping
+# written by Kadir Yaren 
+# ------------- Description -------------
+# use only 60s or more to change your ip otherwise it won't change your ip
+# ------------- Usage -------------
+# python3 tor_reloader.py
+# ------------- Example -------------
+# python3 tor_reloader.py
+# ------------- Version -------------
+# v1.0
+# ------------- Changelog -------------
+# v1.0
+# ------------- License -------------
+# MIT License
+
+import os 
+import time 
+from sys import platform
+import requests
+from colorama import init
+from termcolor import colored
+
+
+def change_ip(reload_time):
+
+    # tor proxy yenilenme aninda istek atmiyor 
+    # bu yuzden yenilerken istek atmasin diye 5 saniye bekliyoruz
+    # ardindan istek atiyoruz
+
+
+    if platform == "linux" or platform == "linux2":
+
+        os.system("service tor reload")
+    elif platform == "darwin":
+        # MACOSX
+        while(True):
+            os.system("brew services reload tor")
+            time.sleep(5)
+            response = requests.get("http://ip-api.com/json/?fields=61439",proxies=dict(http="socks5://127.0.0.1:9050",https="socks5://127.0.0.1:9050")).json()["query"]
+            print(colored(f"Reloaded Tor! Now Your Ip is {response}", 'green', 'on_red'))
+       
+            time.sleep(reload_time-5)
+    else:
+        print("Your OS is not supported")
+    
+    
+
+
+if __name__=="__main__":
+    init() # for colorama
+    TIME = 60
+    change_ip(TIME)
+
+
+# ==> Successfully started `tor` (label: homebrew.mxcl.tor)
+# Service `tor` already started, use `brew services restart tor` to restart.
+# Stopping `tor`... (might take a while)\n
+# ==> Successfully stopped `tor` (label: homebrew.mxcl.tor)
+# Warning: Service `tor` is not started.
+#output = os.popen('brew services start tor').read()
+
