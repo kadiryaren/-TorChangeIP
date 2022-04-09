58503fc792bfd664d919b952be11e60036c78a8c
kadiryaren <uraniumstar20@gmail.com>
2022-04-10 00:59:14 +0300

    Additional Files Deleted

--- deneme.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-response = requests.get("http://ip-api.com/json/?fields=61439",proxies=dict(http="socks5://127.0.0.1:9050",https="socks5://127.0.0.1:9050")).json()["query"]
-print(response)
